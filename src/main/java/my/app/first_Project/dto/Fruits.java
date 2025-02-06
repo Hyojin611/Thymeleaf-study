c528c4ac1d772fedae80d3b0654b9eae9615b9df
Hyojin611 <611gywls@gmail.com>
2025-02-06 14:39:29 +0900

    -자료구조처리

---
+++ src/main/java/my/app/first_Project/dto/Fruits.java
@@ -0,0 +1,15 @@
+package my.app.first_Project.dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Fruits {
+    private String fruitsName;
+    private String taste;
+
+
+}
